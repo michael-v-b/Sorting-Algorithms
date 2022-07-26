c01452eaff55439b34218b67540e970bdfc872a6
Michael Burton <70928683+Churg@users.noreply.github.com>
2022-07-26 16:27:05 -0400

    Final Asset Upload

---
+++ volumeSlider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class volumeSlider : MonoBehaviour
+{
+    public audioManager am;
+    AudioSource audioSource;
+    [SerializeField] TextMeshProUGUI volNum;
+    [SerializeField] Slider volSlider;
+    [SerializeField]
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        am = audioManager.main;
+        audioSource = am.gameObject.GetComponent<AudioSource>();
+        volSlider.value = audioSource.volume;
+        volNum.text = "" + (((float)volSlider.value * 100) - (((float)volSlider.value * 100) % 1));
+
+    }
+    public void changeVolume()
+    {
+        audioSource.volume = volSlider.value;
+        volNum.text = "" + (((float)volSlider.value * 100) - (((float)volSlider.value * 100) % 1));
+    }
+
+
+}
